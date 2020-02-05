7784359add3090a318a4d19c9ee182da94e4aee6
root <root@LAPTOP-CEF3OEJ9.localdomain>
2020-02-05 14:46:20 -0500

    fixed in contaner

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 """uses employee ID to output todo list"""
 
-import requests
-import sys
 
 if __name__ == '__main__':
-    id = sys.argv[1]
-    info = request.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(id))
+    import requests
+    import sys
+    userID = sys.argv[1]
+    info = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(userID))
     new_tasks = response.json()
     comp_tasks = [task for task in new_tasks if task.get('completed') is True]
     EMPLOYEE_NAME = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}'
-        .format(id)).json().get('name')
+        .format(userID)).json().get('name')
     NUMBER_OF_DONE_TASKS = len(comp_tasks)
-    TOTAL_NEMBER_OF_TASKS = len(new_tasks)
+    TOTAL_NUMBER_OF_TASKS = len(new_tasks)
     print("Employee {} is done with tasks({}/{}:"
           .format(
               EMPLOYEE_NAME,
